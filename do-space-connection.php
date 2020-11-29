81a5896f8f79ab8ba1dda7f6e0688b8f61a5b922
Janson-L <52344189+Janson-L@users.noreply.github.com>
2020-11-29 18:21:34 +0800

    Uploaded to specific folder
    -Need to check the format again
    -Need to fix the time, it's 7 hours behind our time

--- do-space-connection.php
+++ do-space-connection.php
@@ -5,14 +5,13 @@ require 'vendor/autoload.php';
 use Aws\S3\S3Client;
 
 $bucketName='utem-traffic-enforcement-sytstem';
-$key='JRSZSJZZSGM4GUEUTEJT';
 
 $client = new Aws\S3\S3Client([
         'version' => 'latest',
         'region'  => 'frankfurt-1',
         'endpoint' => 'https://fra1.digitaloceanspaces.com',
         'credentials' => [
-                'key'    => $key,
+                'key'    => 'JRSZSJZZSGM4GUEUTEJT',
                 'secret' => 'ElYMKwuYSRygUy4zguSoNPkGvFrTCcf/rIkW4x46e8s ',
             ],
 ]);
--- do-space-upload.php
+++ do-space-upload.php
@@ -1,9 +1,13 @@
 <?php
+    $uploadImage='C:\Users\Janson Liew\Desktop\Test Car Cropped.jpg';
+    $currentDateTime=date('Y-m-d_H:i:s');
+    $summonID=1;
     $client->putObject([
         'Bucket' => $bucketName,
-        'Key'    => $key,
+        //'Key'    => 'offence-images/' .basename($uploadImage),
+        'Key'    => 'offence-images/'.$summonID.'_'.$currentDateTime,
         //'Body'   => 'The contents of the file.', //used when you want to use body to upload the file
-        'SourceFile'=>'C:\Users\Janson Liew\Desktop\Test Car Cropped.jpg', //when you want to upload using the specified directory 
+        'SourceFile'=>$uploadImage, //when you want to upload using the specified directory 
         'ACL'    => 'private'
     ]);
 ?>
