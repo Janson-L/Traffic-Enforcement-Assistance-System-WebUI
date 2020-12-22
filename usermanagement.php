ff36fb9061cf4c3e522a60687d5851befc5a47b0
Janson-L <52344189+Janson-L@users.noreply.github.com>
2020-12-23 01:08:57 +0800

    Fixed some UI bug and modularized footer and nav bar

---
+++ usermanagement.php
@@ -0,0 +1,37 @@
+<?php
+include "connection.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>User Management</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <?php include "navbar.php"; ?>
+    <!-- start dekat sini i edit-->
+
+    <div class="container-fluid text-center">
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <!--    Letak gambar dekat sini  -->
+
+                <img src="image/Logo_Polis_Bantuan-01.png" style="height:100px;width: auto;margin: 0 auto;display: block;">
+            </div>
+        </div>
+    </div>
+
+
+    <?php include "footer.php"; ?>
+
+</body>
+
+</html>
