ad686070916eb9209a55e10ab7bf0e80c75d0820
Janson-L <52344189+Janson-L@users.noreply.github.com>
2020-12-24 00:48:02 +0800

    Close connection before navigating

--- check-login.php
+++ check-login.php
@@ -18,25 +18,24 @@ if($check > 0){
 	if($data['Class']=="1"){
 		$_SESSION['StaffID'] = $StaffID;
 		$_SESSION['Class'] = "1";
+		$con->close();
 		header("location:officer.php");
     }
     
     else if($data['Class']=="2"){
 		$_SESSION['StaffID'] = $StaffID;
 		$_SESSION['Class'] = "2";
-		
+		$con->close();
         header("location:commander.php");
     }
     
     else{
+		$con->close();
 		header("location:login.php?message=failed");
 	}
 
-	
 }else{
+	$con->close();
 	header("location:login.php?message=failed");
 }
-
-
-
 ?>
