5e850e2c91fc866e50ee0fba36e86211f33322a7
Shivedhassen Balasingam <shivedhassen@users.noreply.github.com>
2020-12-26 15:50:05 +0800

    Updated login.php
    Retrieve hashed password with password_verify()

--- check-login.php
+++ check-login.php
@@ -6,13 +6,20 @@ include 'connection.php';
 $StaffID = $_POST['StaffID'];
 $Password = $_POST['Password'];
 
-$login = mysqli_query($con,"select * from staff where StaffID='$StaffID' and Password='$Password'");
+$sql="SELECT * FROM staff WHERE StaffID=?";
 
-$check = mysqli_num_rows($login);
+$stmt=mysqli_stmt_init($con);
+mysqli_stmt_prepare($stmt, $sql);
+mysqli_stmt_bind_param($stmt, "s", $StaffID);
+mysqli_stmt_execute($stmt);
 
-if($check > 0){
+$result=mysqli_stmt_get_result($stmt);
 
-	$data = mysqli_fetch_assoc($login);
+$check = mysqli_num_rows($result);
+
+if($result > 0){
+
+	$data = mysqli_fetch_assoc($result);
 
 	if($data['Class']=="1"){
 		$_SESSION['StaffID'] = $StaffID;
