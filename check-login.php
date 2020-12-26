f9ee19cb1f9ed6288ed6fbfb06f4c8cadc20dc91
Janson-L <52344189+Janson-L@users.noreply.github.com>
2020-12-26 21:06:00 +0800

    Fix redirection error

--- check-login.php
+++ check-login.php
@@ -17,7 +17,7 @@ $result=mysqli_stmt_get_result($stmt);
 if($data=mysqli_fetch_assoc($result)){
 	$passwordCheck=password_verify($Password, $data['Password']);
 	if($passwordCheck==false) {
-		header("Location:../login.php?error=wrongpassword");
+		header("Location:login.php?error=wrongpassword");
 		$con->close();
 		exit();
 	}
