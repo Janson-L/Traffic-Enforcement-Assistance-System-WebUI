b3ce4d97c95906c6fe2e2c955bc8b2efc828462d
Janson-L <52344189+Janson-L@users.noreply.github.com>
2020-12-28 02:18:02 +0800

    Satria Resolution is complete

--- resolveWithSummon.php
+++ resolveWithSummon.php
@@ -20,41 +20,41 @@ if (isset($_SESSION['StaffID'])) {
         <?php include "navbar-footer/navbarCommander.php";
 
         if (isset($_POST['resolveWithSummon'])) {
-            // include "connection.php";
-            // $timeOffset='08:00';
+            include "connection.php";
+            $timeOffset='08:00';
             $photoDirectory='1';
-            // $licensePlate = $_POST['LicensePlate'];
-            // $offenseID=2;
-            // $staffID=$_SESSION['StaffID'];
+            $licensePlate = $_POST['LicensePlate'];
+            $offenseID=2;
+            $staffID=$_SESSION['StaffID'];
             
             
-            // $query = 'INSERT INTO summon (`SummonID`, `SummonDateTime`, `PhotoDirectory`, `OffenseID`,`LicensePlate`,`StaffID`) 
-            // VALUES (NULL,ADDTIME(CURRENT_TIMESTAMP(), ?),?,?,?,?);';
-            // $stmt = mysqli_stmt_init($con);
-            // mysqli_stmt_prepare($stmt, $query);
-            // mysqli_stmt_bind_param($stmt, "sssss", $timeOffset,$photoDirectory,$offenseID,$licensePlate,$staffID);
-            // mysqli_stmt_execute($stmt);
-            // mysqli_stmt_close($stmt);
+            $query = 'INSERT INTO summon (`SummonID`, `SummonDateTime`, `PhotoDirectory`, `OffenseID`,`LicensePlate`,`StaffID`) 
+            VALUES (NULL,ADDTIME(CURRENT_TIMESTAMP(), ?),?,?,?,?);';
+            $stmt = mysqli_stmt_init($con);
+            mysqli_stmt_prepare($stmt, $query);
+            mysqli_stmt_bind_param($stmt, "sssss", $timeOffset,$photoDirectory,$offenseID,$licensePlate,$staffID);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_close($stmt);
 
-            // $query = 'SELECT `SummonID`,`SummonDateTime`
-            // FROM summon ORDER BY `SummonID` DESC LIMIT 1';
-            // $stmt = mysqli_stmt_init($con);
-            // mysqli_stmt_prepare($stmt, $query);
-            // mysqli_stmt_execute($stmt);
-            // mysqli_stmt_bind_result($stmt, $summonID,$summonDateTime);
-            // mysqli_stmt_fetch($stmt);
-            // mysqli_stmt_close($stmt);
+            $query = 'SELECT `SummonID`,`SummonDateTime`
+            FROM summon ORDER BY `SummonID` DESC LIMIT 1';
+            $stmt = mysqli_stmt_init($con);
+            mysqli_stmt_prepare($stmt, $query);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_bind_result($stmt, $summonID,$summonDateTime);
+            mysqli_stmt_fetch($stmt);
+            mysqli_stmt_close($stmt);
 
-            // $summonDateTime= str_replace(" ","_",$summonDateTime);
-            // $photoDirectory=$summonID.'_'.$summonDateTime;
+            $summonDateTime= str_replace(" ","_",$summonDateTime);
+            $photoDirectory=$summonID.'_'.$summonDateTime;
 
-            // $query = 'UPDATE `summon` SET`PhotoDirectory`= ? WHERE `SummonID`=?';
-            // $stmt = mysqli_stmt_init($con);
-            // mysqli_stmt_prepare($stmt, $query);
-            // mysqli_stmt_bind_param($stmt, "si", $photoDirectory, $summonID);
-            // mysqli_stmt_execute($stmt);
-            // mysqli_stmt_close($stmt);
-            // mysqli_close($con);
+            $query = 'UPDATE `summon` SET`PhotoDirectory`= ? WHERE `SummonID`=?';
+            $stmt = mysqli_stmt_init($con);
+            mysqli_stmt_prepare($stmt, $query);
+            mysqli_stmt_bind_param($stmt, "si", $photoDirectory, $summonID);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_close($stmt);
+            mysqli_close($con);
         ?>
             <div class="container-fluid text-center">
                 <div class="row">
