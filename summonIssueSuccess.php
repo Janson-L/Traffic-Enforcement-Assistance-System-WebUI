1319754b877a9dbf5b3e1696d48248467185d8ce
Janson-L <52344189+Janson-L@users.noreply.github.com>
2020-12-28 02:13:43 +0800

    Update summonIssueSuccess.php

--- summonIssueSuccess.php
+++ summonIssueSuccess.php
@@ -2,28 +2,31 @@
 SESSION_START();
 if (isset($_SESSION['StaffID'])) {
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Success</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style/style.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-</head>
-<body>
-<?php include "navbar-footer/navbarCommander.php" ?>
-<br>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Success</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="style/style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    </head>
+
+    <body>
+        <?php
+        include "navbar-footer/navbarCommander.php";
+        $_SESSION['ResolutionOrigin'] = '';
+        ?>
+        <br>
         <div class="prompt">Summon successfully issued. You will now be redirected in 5 seconds.</div>
-    <?php
-            header("Refresh:5;URL=resolve.php");
-            die();   
-    ?>
-<?php
-    include "navbar-footer/footer.php"
-    ?>
+        <?php
+        include "navbar-footer/footer.php";
+        header("Refresh:5;URL=resolve.php");
+        die();
+        ?>
     </body>
 
     </html>
