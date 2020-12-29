e2ae9448966b7c2b6e2cde619f3828b41f3364c2
Shivedhassen Balasingam <shivedhassen@users.noreply.github.com>
2020-12-29 23:24:12 +0800

    Update paymentmethod.php

--- paymentmethod.php
+++ paymentmethod.php
@@ -41,18 +41,54 @@ $query = "SELECT summon.SummonID, summon.SummonDateTime, student.Name, vehicle.S
                 join student ON vehicle.StudentID = student.StudentID
                 WHERE summon.SummonID = $editRow";
 $result = mysqli_query($con, $query);
-    echo '<table class="centerthistable"> <tr><th>SummonID</th><th>Date and Time</th><th>Student Name</th><th>Matrix Number</th><th>Number Plate</th><th>Offense</th><th>Compound(RM)</th></tr>';
-    while ($row = mysqli_fetch_assoc($result)) {
-      echo "<tr>";
-      echo "<td>" . $row['SummonID'] . "</td>";
+    while ($row = mysqli_fetch_assoc($result)) { ?>
+
+<form name="ConfirmPayment" action="paymentmethod.php" method="post">
+	<input type="hidden" name="SummonID" value="<?php echo $row["SummonID"]; ?>"/>        
+	<table border="0">
+	<col width="200">
+            <tr>
+                <td><h3><label>Name</label></h3></td>
+                <td><input type="text" name="Name" value="<?php echo $row["Name"] ?>" ></input></td>
+            </tr>
+            <tr>
+                <td><h3><label>Matrics Number</label></h3></td>
+                <td><input type="text" name="StudentID" value="<?php echo $row["StudentID"] ?>" ></input></td>
+            </tr>
+	    <tr>
+                <td><h3><label></label></h3></td>
+                <td><input type="text" name="Billing_address" value="<?php echo $_POST["Billing_address"] ?>"></input></td>
+            </tr>
+	    <tr>
+                <td><h3><label>Email Address</label></h3></td>
+                <td><input type="email" name="Email_address" value="<?php echo $_POST["Email_address"] ?>"></input></td>
+            </tr>
+            <tr>
+		
+                <td><input type="submit" name="submit" value="Save" />
+    		
+            </tr>
+	<tr>
+		
+            <h4>Upon saving please verify your email account by accepting our mail</h3>		
+            </tr>
+        </table>
+ </form>
+
+     <!--  echo "<tr>";
+      echo "<td>" . $row['SummonID'] . "</input></td>";
       echo "<td>" . $row['SummonDateTime'] . "</td>";
       echo "<td>" . $row['Name'] . "</td>";
       echo "<td>" . $row['StudentID'] . "</td>";
       echo "<td>" . $row['LicensePlate'] . "</td>";
       echo "<td>" . $row['OffenseName'] . "</td>";
-      echo "<td>" . $row['CompoundRate'] . "</td>";
+      echo "<td>" . $row['CompoundRate'] . "</td>"; -->
+    <?php
     }
-?>
+    ?>
+
+
+
 
 <?php
 }
