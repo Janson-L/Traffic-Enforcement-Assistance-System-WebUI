5af9fcd64344f476315ddacca4a54d2e4d1420fa
Janson-L <52344189+Janson-L@users.noreply.github.com>
2021-01-06 03:07:09 +0800

    Updated webcam code

--- testcamera/index.php
+++ testcamera/index.php
@@ -7,7 +7,7 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
+    <script src="/Traffic-Enforcement-Assistance-System/js/webcam.js"></script>
 
     <style type="text/css">
 
@@ -60,7 +60,14 @@
         image_format: 'jpeg',
         jpeg_quality: 100
     });
-
+    
+    Webcam.on("init", function () {
+        Webcam.getCameras(function (cameras) {
+          if (cameras.length > 0) {
+            Webcam.setAndInitCamera(cameras[cameras.length - 1].id);
+          }
+        });
+      });
     Webcam.attach( '#my_camera' );
 
     function take_snapshot() {
