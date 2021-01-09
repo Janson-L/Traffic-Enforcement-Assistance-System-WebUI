814d73e716cc7ce10be5ab90b1c614b63ea17564
Shivedhassen Balasingam <shivedhassen@users.noreply.github.com>
2021-01-09 22:48:11 +0800

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
-<link rel="stylesheet" href="style.css">
-<title>Add New Staff</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Traffic Enforcement Assistance System</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="style/style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="style.css">
+	
+	<title>Add New Staff</title>
 </head>
 <body>
 <div class="register-box">
